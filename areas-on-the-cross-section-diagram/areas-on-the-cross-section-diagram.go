ba6f8a651b44cf60e200d5f9817c61bf69337669
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-05-01 00:18:49 +0900

    Fix miss

--- areas-on-the-cross-section-diagram/areas-on-the-cross-section-diagram.go
+++ areas-on-the-cross-section-diagram/areas-on-the-cross-section-diagram.go
@@ -21,17 +21,23 @@ func main() {
 		if string(signal) == `\` { // \ であれば、stackに積む
 			leftSignalPositionStack.Push(nowPosition)
 		} else if string(signal) == "/" {
+
+			if len(leftSignalPositionStack.list) <= 0 {
+				continue
+			}
+
 			// leftSignalPositionStackのトップを取り出す
 			top := leftSignalPositionStack.Pop().(int)
 
 			// 今回の面積を求める
-			area := top - nowPosition
+			area := nowPosition - top
+
 			// 今回の面積を総合面積に追加する
 			grossArea += area
 
-			for i := len(nodes.list) - 1; i >= 0; i-- {
-				node := nodes.Pop().(*Node)
-				if top < node.Position {
+			for len(nodes.list) > 0 {
+				if top < nodes.list[len(nodes.list)-1].(*Node).Position {
+					node := nodes.Pop().(*Node)
 					area += node.Area // 新たにできた水たまりの面積を直線までの面積に加える
 				} else {
 					break
@@ -39,7 +45,7 @@ func main() {
 			}
 			// 各水たまりに加える
 			// 各々水たまりも、\と/の数が合う度に面積を加えていく必要がある
-			nodes.Push(&Node{top, area})
+			nodes.Push(&Node{Area: area, Position: top})
 		}
 	}
 
@@ -74,18 +80,18 @@ func getInput() string {
 
 // Stack は、スタック構造を表す。
 type Stack struct {
-	top  int // top は、最後の要素が格納されている場所を表す。
 	list []interface{}
 }
 
 // Push は、スタックのトップに引数で与えられた数字を追加する。
 func (s *Stack) Push(el interface{}) {
-	s.top++
-	s.list[s.top] = el
+	s.list = append(s.list, el)
 }
 
 // Pop は、スタックのトップの数字を返す。
-func (s *Stack) Pop() interface{} {
-	s.top--
-	return s.list[s.top+1]
+func (s *Stack) Pop() (target interface{}) {
+	length := len(s.list)
+	target = s.list[length-1]
+	s.list = s.list[:length-1]
+	return
 }
