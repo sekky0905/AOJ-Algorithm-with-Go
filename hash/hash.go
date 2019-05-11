ee57159b95c792af1f55ff1ac2bb3686c67ef5a1
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-05-12 01:58:32 +0900

    Tiny up

--- hash/hash.go
+++ hash/hash.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"strconv"
@@ -15,20 +16,7 @@ const (
 	value
 )
 
-func main() {
-	n := scanToInt()
-	h := hashTable{}
-
-	for i := 0; i < n; i++ {
-		str := scanToString()
-		s := strings.Split(str, " ")
-		if s[method] == "insert" {
-			h.insert(s[value])
-		} else {
-			fmt.Print(boolToString(h.isExist(s[value])))
-		}
-	}
-}
+var length = 1000
 
 func scanToInt() int {
 	sc.Scan()
@@ -44,71 +32,61 @@ func scanToString() string {
 	return sc.Text()
 }
 
-func boolToString(b bool) string {
-	if b {
-		return "yes"
-	}
-	return "no"
-}
-
-// hashTable は、ハッシュテーブルを表す。
-type hashTable map[int]string
-
 // insert は、ハッシュテーブルにデータを格納する。
-func (h hashTable) insert(str string) {
+func insert(hashTable []string, str string) {
 	i := 0
 	key := getKeyFromString(str)
 	for {
-		hashed := h.hash(key, i)
-		if h[hashed] == "" { // 指定した箇所に何も入っていなかったら、格納
-			h[hashed] = str
-			continue
+		hashed := hash(key, i)
+		if hashTable[hashed] == "" { // 指定した箇所に何も入っていなかったら、格納
+			hashTable[hashed] = str
+			return
 		}
 		i++ // hashのiをインクリメントする
 	}
 }
 
 // isExist は、引数で与えられた文字列がhashTable上に既に存在するかどうかを確認する。
-func (h hashTable) isExist(str string) bool {
+func isExist(hashTable []string, str string) bool {
 	i := 0
 	key := getKeyFromString(str)
 	for {
-		hashed := h.hash(key, i)
-		if h[hashed] == str {
+		hashed := hash(key, i)
+		if hashTable[hashed] == str {
 			return true
-		} else if h[hashed] == "" || i >= len(h) { // i毎にhashでズラして進めていく中でから文字が存在したら、それ以上進めても存在しないので
+		} else if hashTable[hashed] == "" || i >= length { // i毎にhashでズラして進めていく中でから文字が存在したら、それ以上進めても存在しないので
 			return false
 		}
 		i++
 	}
 }
 
+// hash は、hashTableに対するハッシュ値を取得する。
+func hash(key, i int) int {
+	return (hash1(key) + i*hash2(key)) % length
+}
+
 // hash1 は、引数で与えられたkeyから、key mod len(hashTable)を計算する。
-func (h hashTable) hash1(key int) int {
+func hash1(key int) int {
 	// len(hashTable) = mとする時に、0~m-1になるようにする。
-	return key % len(h)
+	return key % length
 }
 
 // hash2 は、与えられたkeyとiから、1 + (key mod len(hashTable) -1) を計算する。
-func (h hashTable) hash2(key int) int {
-	return 1 + (key % (len(h) - 1))
-}
-
-// hash は、hashTableに対するハッシュ値を取得する。
-func (h hashTable) hash(key, i int) int {
-	return (h.hash1(key) + i*h.hash2(key)) % len(h)
+func hash2(key int) int {
+	return 1 + (key % (length - 1))
 }
 
 // 1文字を数字に変換する。
-func getIntFromChar(char string) int {
+func getIntFromChar(char byte) int {
 	switch char {
-	case "A":
+	case 'A':
 		return 1
-	case "C":
+	case 'C':
 		return 2
-	case "G":
+	case 'G':
 		return 3
-	case "T":
+	case 'T':
 		return 4
 	default:
 		panic("invalid char")
@@ -120,11 +98,32 @@ func getKeyFromString(str string) int {
 	const adjusterValue = 5
 	sum, adjuster := 0, 1
 
-	for _, s := range str {
-		sum += adjuster * (getIntFromChar(string(s)))
+	for i := range str {
+		sum += adjuster * (getIntFromChar(str[i]))
 		// 例えば、AATの場合6になる。
 		// CCCの場合も6になり、衝突してしまうので各値に下駄を履かせる
 		adjuster *= adjusterValue
 	}
 	return sum
+}
+
+func main() {
+	length = scanToInt()
+	// ハッシュテーブル
+	hashTable := make([]string, length)
+
+	buf := bytes.Buffer{}
+	for i := 0; i < length; i++ {
+		s := strings.Split(scanToString(), " ")
+		if s[method] == "insert" {
+			insert(hashTable, s[value])
+		} else {
+			if isExist(hashTable, s[value]) {
+				buf.WriteString("yes\n")
+			} else {
+				buf.WriteString("no\n")
+			}
+		}
+	}
+	fmt.Println(strings.TrimRight(buf.String(), "\n"))
 }
