234252bacbdafb79c3c3a39ddd46c382e8cffe8a
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-05-29 00:01:18 +0900

    Add compare

--- quick_sort/quick_sort.go
+++ quick_sort/quick_sort.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
+	"reflect"
 	"strconv"
+	"strings"
 )
 
 // card は、カードを表す。
 type card struct {
 	suit string
-	Num  int
+	num  int
 }
 
 var sc = bufio.NewScanner(os.Stdin)
@@ -21,6 +25,11 @@ func scanToInt() int {
 		panic(err)
 	}
 	return n
+}
+
+func scanToString() string {
+	sc.Scan()
+	return sc.Text()
 }
 
 // quickSort は、クリックソートを行う。
@@ -38,11 +47,11 @@ func quickSort(cards []*card, p, r int) {
 // partition は、パーティションを使用しSortする。
 // パーティションの対象は、p〜rとする。
 func partition(cards []*card, p, r int) int {
-	x := cards[r].Num
+	x := cards[r].num
 	i := p - 1
 
 	for j := p; j < r; j++ {
-		if cards[j].Num <= x {
+		if cards[j].num <= x {
 			i++
 			cards[i], cards[j] = cards[j], cards[i]
 		}
@@ -51,11 +60,96 @@ func partition(cards []*card, p, r int) int {
 	return i + 1
 }
 
+// merge は、マージを行う。
+func merge(cards []*card, left, mid, right int) {
+	n1 := mid - left
+	n2 := right - mid
+
+	lLen, rLen := n1+1, n2+1
+	leftCards, rightCards := make([]*card, lLen, lLen), make([]*card, rLen, rLen)
+
+	// 左側のSliceを作成する
+	for i := 0; i < n1; i++ {
+		leftCards[i] = cards[left+i]
+	}
+
+	// 番兵としてありえない数字を置く
+	dummy := &card{
+		suit: "",
+		num:  math.MaxInt32,
+	}
+	leftCards[n1] = dummy
+
+	// 右側のSliceを作成する
+	for i := 0; i < n2; i++ {
+		rightCards[i] = cards[mid+i]
+	}
+
+	// 番兵としてありえない数字を置く
+	rightCards[n2] = dummy
+
+	i, j := 0, 0
+
+	// 左側のSliceと右側のSliceを比較しながら元のSliceに格納していく
+	for k := left; k < right; k++ {
+		if leftCards[i].num <= rightCards[j].num {
+			cards[k] = leftCards[i]
+			i = i + 1
+		} else {
+			cards[k] = rightCards[j]
+			j = j + 1
+		}
+	}
+}
+
+// mergeSort は、メージソートを行う。
+func mergeSort(cards []*card, left, right int) {
+	if left+1 < right {
+		mid := (left + right) / 2
+		// 左側の整列
+		mergeSort(cards, left, mid)
+		// 右側の整列
+		mergeSort(cards, mid, right)
+		merge(cards, left, mid, right)
+	}
+}
+
 func main() {
 	n := scanToInt()
 
-	list := make([]int, n, n)
+	a := make([]*card, n, n)
+	b := make([]*card, n, n)
 	for i := 0; i < n; i++ {
-		list[i] = scanToInt()
+		cardStr := scanToString()
+		s := strings.Split(cardStr, " ")
+
+		suit := s[0]
+		num, err := strconv.Atoi(s[1])
+		if err != nil {
+			panic(err)
+		}
+
+		card1 := &card{
+			suit: suit,
+			num:  num,
+		}
+
+		card2 := card1
+
+		a[i] = card1
+		b[i] = card2
+	}
+
+	mergeSort(a, 0, n)
+	quickSort(b, 0, n-1)
+
+	if reflect.DeepEqual(a, b) {
+		fmt.Println("Stable")
+	} else {
+		fmt.Println("Not stable")
+	}
+
+	for _, v := range b {
+		fmt.Printf("%s %d\n", v.suit, v.num)
 	}
 }
