b2ebf38cab0bf74865087eeda9b53801295bdbe0
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-05-28 23:20:08 +0900

    Add input process

--- quick_sort/quick_sort.go
+++ quick_sort/quick_sort.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
 // card は、カードを表す。
 type card struct {
 	suit string
 	Num  int
 }
 
-func main() {
+var sc = bufio.NewScanner(os.Stdin)
 
+func scanToInt() int {
+	sc.Scan()
+	n, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func main() {
+	n := scanToInt()
+
+	list := make([]int, n, n)
+	for i := 0; i < n; i++ {
+		list[i] = scanToInt()
+	}
 }
