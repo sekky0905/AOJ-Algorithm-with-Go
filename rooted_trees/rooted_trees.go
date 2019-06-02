bfb124a584ec28c1e7e2eb8786549f07843672ea
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-02 23:46:57 +0900

    Add rooted trees

--- rooted_trees/rooted_trees.go
+++ rooted_trees/rooted_trees.go
@@ -1,11 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 // node は、木の接点もしくは葉を表す。
 type node struct {
 	id       int
 	parent   int
 	depth    int
-	nodeType string
 	children []int
 }
 
@@ -18,6 +26,85 @@ func setDepth(nodes []*node, currentNodeID, depth int) {
 	}
 }
 
-func main() {
+func bufferingNodeInfo(node *node, buf bytes.Buffer) {
+	nodeType := "leaf"
+	if node.parent == -1 {
+		nodeType = "root"
+	} else if len(node.children) > 0 {
+		nodeType = "internal node"
+	}
 
+	childStr := strings.Replace(fmt.Sprintf("%v", node.children), " ", ", ", -1)
+	buf.WriteString(fmt.Sprintf("node %d: parent = %d, depth = %d, %s, [%s]", node.id, node.parent, node.depth, nodeType, childStr))
+}
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanToInt() int {
+	sc.Scan()
+	n, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func scanToStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+const (
+	idIndex = iota
+	degreeIndex
+)
+
+func main() {
+	// 高さ n
+	height := scanToInt()
+	// 高さ height の tree
+	tree := make([]*node, height)
+
+	previousID := -1
+	for i := 0; i < height; i++ {
+		// 1行ずつ取り出す
+		// 次数ごとの情報
+		row := scanToStr()
+		info := strings.Split(row, " ")
+
+		fmt.Printf("=>%+v", info)
+
+		id, err := strconv.Atoi(info[idIndex])
+		if err != nil {
+			panic(err)
+		}
+
+		degree, err := strconv.Atoi(info[degreeIndex])
+		if err != nil {
+			panic(err)
+		}
+
+		// child node を埋める
+		children := make([]int, degree, degree)
+		for i := 0; i < degree; i++ {
+			if children[i], err = strconv.Atoi(info[i+2]); err != nil { // +2は、id と degree の分
+				panic(err)
+			}
+		}
+
+		tree[i] = &node{
+			parent:   previousID,
+			id:       id,
+			children: children,
+		}
+
+		previousID = id
+	}
+
+	setDepth(tree, 0, 0)
+
+	buf := bytes.Buffer{}
+	for _, node := range tree {
+		bufferingNodeInfo(node, buf)
+	}
 }
