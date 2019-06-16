17c48c229b581590a5b517ed39caab98e92d51bf
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-16 15:28:18 +0900

    add insert method which adds node to binary search tree

--- binary_search_tree_1/binary_search_tree_1.go
+++ binary_search_tree_1/binary_search_tree_1.go
@@ -8,6 +8,34 @@ type node struct {
 	right  *node // 右の子
 }
 
+// root は、二分探索木のrootを表す。
+var root *node
+
+// insert は、treeにnodeを挿入する。
+func insert(z *node) {
+	var y *node // xの親
+	x := root
+
+	for x != nil { // xがnilじゃない、つまり要素が存在する
+		y = x // 親を設定
+		if z.key < x.key {
+			x = x.left // 左の子に進む
+		} else {
+			x = x.right // 右の子に進む
+		}
+	}
+	// z(今回の挿入するnode)の親をyにする
+	z.parent = y
+
+	if y == nil { // yがnilということは、二分探索木は空だということ
+		root = z
+	} else if z.key < y.key {
+		y.left = z // yの左の子にzを設定する
+	} else {
+		y.right = z // yの右の子にzを設定する
+	}
+}
+
 func main() {
 
 }
