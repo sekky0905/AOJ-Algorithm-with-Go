20f50dd31161647d2f9e574d85a7434e2f2a61ef
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-23 20:45:00 +0900

    add get left child node index function

--- complete_binary_tree/complete_binary_tree.go
+++ complete_binary_tree/complete_binary_tree.go
@@ -3,13 +3,13 @@ package main
 type tree []int
 
 // getParentIndex は、indexで指定されたnodeの親nodeのindexを取得する。
-func getParentIndex(index int) int {
+func getParent(index int) int {
 	return index / 2
 }
 
-// getLeft は、indexで指定されたnodeの左の子nodeのkeyを取得する。
-func (t tree) getLeft(index int) int {
-	return t[index*2]
+// getLeft は、indexで指定されたnodeの左の子nodeのkeyのindexを取得する。
+func getLeftIndex(index int) int {
+	return index * 2
 }
 
 // getRight は、indexで指定されたnodeの左の子nodeのkeyを取得する。
