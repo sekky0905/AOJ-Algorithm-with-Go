d2d40446d9ee5b8cce1a3f2eeadb07c8ca4b04f5
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-23 21:53:53 +0900

    add make max heap function

--- maximum_heap/maximum_heap.go
+++ maximum_heap/maximum_heap.go
@@ -10,6 +10,33 @@ func geRightIndex(index int) int {
 	return index*2 + 1
 }
 
+// makeMaxHeap は、treeをindexのnodeをrootとするmax heapにする。
+func makeMaxHeap(tree []int, index int) {
+	leftIndex := getLeftIndex(index)
+	rightIndex := getLeftIndex(index)
+
+	n := len(tree)
+
+	var largest int
+
+	// 自node, 左の子node, 右の子nodeの中で値が最大のnodeをlargestとする
+	if leftIndex <= n && tree[leftIndex] > tree[index] {
+		largest = leftIndex
+	} else {
+		largest = index
+	}
+
+	if rightIndex <= n && tree[rightIndex] > tree[largest] {
+		largest = rightIndex
+	}
+
+	if largest != index { // 自nodeよりも子nodeの方が大きい場合、交換する
+		tree[index], tree[largest] = tree[largest], tree[index]
+		makeMaxHeap(tree, largest)
+	}
+
+}
+
 func main() {
 
 }
