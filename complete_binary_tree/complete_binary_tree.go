12c20a64b00190d94931223730156eac5040bebc
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-23 20:38:14 +0900

    add skeleton of tree

---
+++ complete_binary_tree/complete_binary_tree.go
@@ -0,0 +1,22 @@
+package main
+
+type tree []int
+
+// getParent は、indexで指定されたnodeの親nodeのkeyを取得する。
+func getParent(index int) int {
+	return 0
+}
+
+// getLeft は、indexで指定されたnodeの左の子nodeのkeyを取得する。
+func getLeft() int {
+	return 0
+}
+
+// getRight は、indexで指定されたnodeの左の子nodeのkeyを取得する。
+func geRight() int {
+	return 0
+}
+
+func main() {
+
+}
