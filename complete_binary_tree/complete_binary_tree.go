701e9d6f0b17fd50787f31d79cd9b676b80104fb
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-23 20:44:21 +0900

    fix get parent function

--- complete_binary_tree/complete_binary_tree.go
+++ complete_binary_tree/complete_binary_tree.go
@@ -2,14 +2,14 @@ package main
 
 type tree []int
 
-// getParent は、indexで指定されたnodeの親nodeのkeyを取得する。
-func (t tree) getParent(index int) int {
-	return t[index/2]
+// getParentIndex は、indexで指定されたnodeの親nodeのindexを取得する。
+func getParentIndex(index int) int {
+	return index / 2
 }
 
 // getLeft は、indexで指定されたnodeの左の子nodeのkeyを取得する。
-func getLeft() int {
-	return 0
+func (t tree) getLeft(index int) int {
+	return t[index*2]
 }
 
 // getRight は、indexで指定されたnodeの左の子nodeのkeyを取得する。
