0404d1d6bbdc601b5c6932448e8eb5936bde5f47
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-24 23:35:04 +0900

    add print function

--- maximum_heap/maximum_heap.go
+++ maximum_heap/maximum_heap.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -12,32 +14,34 @@ func getLeftIndex(index int) int {
 }
 
 // getRightIndex は、indexで指定されたnodeの左の子nodeのindexを取得する。
-func geRightIndex(index int) int {
+func getRightIndex(index int) int {
 	return index*2 + 1
 }
 
 // makeMaxHeap は、treeをindexのnodeをrootとするmax heapにする。
 func makeMaxHeap(tree []int, index int) {
 	leftIndex := getLeftIndex(index)
-	rightIndex := getLeftIndex(index)
+	rightIndex := getRightIndex(index)
 
 	n := len(tree)
 
 	var largest int
 
 	// 自node, 左の子node, 右の子nodeの中で値が最大のnodeをlargestとする
-	if leftIndex <= n && tree[leftIndex] > tree[index] {
+	if leftIndex < n && tree[leftIndex] > tree[index] {
 		largest = leftIndex
 	} else {
 		largest = index
 	}
 
-	if rightIndex <= n && tree[rightIndex] > tree[largest] {
+	if rightIndex < n && tree[rightIndex] > tree[largest] {
 		largest = rightIndex
 	}
 
 	if largest != index { // 自nodeよりも子nodeの方が大きい場合、交換する
-		tree[index], tree[largest] = tree[largest], tree[index]
+		tmp := tree[index]
+		tree[index] = tree[largest]
+		tree[largest] = tmp
 		makeMaxHeap(tree, largest)
 	}
 
@@ -49,6 +53,14 @@ func buildMaxHeap(tree []int) {
 		makeMaxHeap(tree, i)
 	}
 
+}
+
+func print(tree []int) {
+	var buf bytes.Buffer
+	for i := 1; i < len(tree); i++ {
+		buf.WriteString(fmt.Sprintf(" %d", tree[i]))
+	}
+	fmt.Println(buf.String())
 }
 
 var sc = bufio.NewScanner(os.Stdin)
@@ -71,4 +83,8 @@ func main() {
 		v := scanToInt()
 		tree[i] = v
 	}
+
+	buildMaxHeap(tree)
+
+	print(tree)
 }
