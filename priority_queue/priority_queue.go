ee584af9787878738b25cdc3670ce14bfa0c4541
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-24 21:07:28 +0900

    add function to get right child index

--- priority_queue/priority_queue.go
+++ priority_queue/priority_queue.go
@@ -10,6 +10,11 @@ func getLeftIndex(index int) int {
 	return index * 2
 }
 
+// getRightIndex は、indexで指定されたnodeの左の子nodeのindexを取得する。
+func geRightIndex(index int) int {
+	return index*2 + 1
+}
+
 func main() {
 
 }
