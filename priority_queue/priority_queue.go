a7ef9da5549e41cdeee9c93c83c1497a1e4bc238
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-24 21:44:25 +0900

    add heapIncreaseKey function

--- priority_queue/priority_queue.go
+++ priority_queue/priority_queue.go
@@ -20,6 +20,24 @@ func isValidIndex(index, n int) bool {
 	return index <= n && index > 0
 }
 
+// heapIncreaseKey は、treeのindexで指定されたnode以上の適切な位置にkeyを格納する。
+func heapIncreaseKey(tree []int, index, key int) {
+	// keyが現在のkeyよりも小さい場合は、return
+	if key < tree[index] {
+		return
+	}
+
+	// keyをtreeに格納する
+	tree[index] = key
+
+	// 今回のnodeから、rootまで親を辿って遡る
+	// 親よりも今回のkeyの方が大きい場合、親と入れ替える
+	for index > 1 && tree[getParentIndex(index)] < tree[index] {
+		tree[index], tree[getParentIndex(index)] = tree[getParentIndex(index)], tree[index]
+		index = getParentIndex(index)
+	}
+}
+
 func main() {
 
 }
