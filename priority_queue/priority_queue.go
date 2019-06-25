1141614a379ebba4874b8e9d04672f8b681a4152
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-06-25 21:21:03 +0900

    add function to make max heap

--- priority_queue/priority_queue.go
+++ priority_queue/priority_queue.go
@@ -13,13 +13,50 @@ func getLeftIndex(index int) int {
 }
 
 // getRightIndex は、indexで指定されたnodeの左の子nodeのindexを取得する。
-func geRightIndex(index int) int {
+func getRightIndex(index int) int {
 	return index*2 + 1
 }
 
 // isValidIndex は与えられたindexが有効なものかどうかを確認する。
 func isValidIndex(index, n int) bool {
 	return index <= n && index > 0
+}
+
+// makeMaxHeap は、treeをindexのnodeをrootとするmax heapにする。
+func makeMaxHeap(tree []int, index int) {
+	leftIndex := getLeftIndex(index)
+	rightIndex := getRightIndex(index)
+
+	n := len(tree)
+
+	var largest int
+
+	// 自node, 左の子node, 右の子nodeの中で値が最大のnodeをlargestとする
+	if leftIndex < n && tree[leftIndex] > tree[index] {
+		largest = leftIndex
+	} else {
+		largest = index
+	}
+
+	if rightIndex < n && tree[rightIndex] > tree[largest] {
+		largest = rightIndex
+	}
+
+	if largest != index { // 自nodeよりも子nodeの方が大きい場合、交換する
+		tmp := tree[index]
+		tree[index] = tree[largest]
+		tree[largest] = tmp
+		makeMaxHeap(tree, largest)
+	}
+
+}
+
+func buildMaxHeap(tree []int) {
+	// 子nodeを持つ最大のnodeのindexは、len(tree) / 2
+	for i := len(tree) / 2; i >= 1; i-- {
+		makeMaxHeap(tree, i)
+	}
+
 }
 
 // insert は、treeの適切な位置にkeyを格納する。
