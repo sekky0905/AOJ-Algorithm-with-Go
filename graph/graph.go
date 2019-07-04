c99dcf3127093a838f1b69b95ae2ba9077289d3a
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-07-05 00:04:47 +0900

    add function to scan input number

--- graph/graph.go
+++ graph/graph.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
 // adjacentMatrix は、隣接行列を表す。
 var adjacentMatrix [][]int
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanToInt() int {
+	sc.Scan()
+	n, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
 
 func main() {}
