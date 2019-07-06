8fc8c05dfa61bbf22cfa8422b8ccaf573c39a140
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-07-06 22:50:00 +0900

    add function to deep first search

--- depth_first_search/recursive_ver/depth_first_search.go
+++ depth_first_search/recursive_ver/depth_first_search.go
@@ -4,17 +4,17 @@ const maxLength = 100
 
 var (
 	// adjacentMatrix は、隣接行列を表す。
-	adjacentMatrix [][]int
+	adjacentMatrix [][]bool
 	// timeCounter は全体の時刻を表す。
-	timeCounter int
+	timeCounter int = 0
 )
 
 type color string
 
 const (
-	white color = "WHITE"
-	gray  color = "GRAY"
-	black color = "BLACK"
+	white color = "WHITE" // 訪問前のを表す
+	gray  color = "GRAY"  // 訪問したことを表す
+	black color = "BLACK" // 完了を表す
 )
 
 // node は、グラフにおける頂点を表す。
@@ -22,6 +22,33 @@ type node struct {
 	color
 	foundTime     int
 	completedTime int
+}
+
+var nodes []node
+
+// deepFirstSearch は、深さ優先探索を行う。
+func deepFirstSearch(n, u int) {
+	// 今回訪問した
+	timeCounter++
+	nodes[u] = node{
+		color:     gray,
+		foundTime: timeCounter,
+	}
+
+	for i := 0; i < n; i++ {
+		if !adjacentMatrix[u][i] { // 隣接行列の今回の箇所に存在しない場合
+			continue
+		}
+		if nodes[i].color == white { // 未踏なら再帰的にその頂点から深さ優先探索を行う
+			deepFirstSearch(n, i)
+		}
+	}
+	// 完了した
+	timeCounter++
+	nodes[u] = node{
+		color:     black,
+		foundTime: timeCounter,
+	}
 }
 
 func main() {
