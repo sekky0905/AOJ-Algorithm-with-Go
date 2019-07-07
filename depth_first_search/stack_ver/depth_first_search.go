37f425b6a00ccc2629cd9461aa727cabee461965
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-07-07 18:58:41 +0900

    add function to deep first search visit

--- depth_first_search/stack_ver/depth_first_search.go
+++ depth_first_search/stack_ver/depth_first_search.go
@@ -22,6 +22,7 @@ const (
 
 // node は、グラフにおける頂点を表す。
 type node struct {
+	value int
 	color
 	foundTime     int
 	completedTime int
@@ -38,9 +39,64 @@ func (s simpleStack) push(v int) {
 func (s simpleStack) pop() (int, error) {
 	length := len(s)
 	if length < 1 {
-		return 0, errors.New("index out of range")
+		return -1, errors.New("index out of range")
+	}
+	stack[len(stack)-1] = 0
+}
+
+func (s simpleStack) top() (int, error) {
+	length := len(s)
+	if length < 1 {
+		return -1, errors.New("index out of range")
 	}
 	return s[length-1], nil
+}
+
+var stack simpleStack
+
+// deepFirstSearchVisit は、深さ優先探索で訪問する。
+func deepFirstSearchVisit(u int) error {
+	stack.push(u) // 訪問する頂点uをスタックに追加
+
+	// 頂点uを訪問する
+	timeCounter++
+	nodes[u].color = gray // 訪問したのでuのcolorをgray(訪問済みにする)
+	nodes[u].foundTime = timeCounter
+
+	for len(stack) != 0 {
+		u, err := stack.pop()
+		if err != nil {
+			return err
+		}
+
+		// 隣接頂点を取得する
+		v := getNextNode(u)
+		if v != -1 { // 隣接頂点が見つかったら
+			if nodes[v].color == white { // 番号順に取得した頂点を訪問する
+				timeCounter++
+				nodes[v].color = gray
+				nodes[v].foundTime++
+				stack.push(v) // 頂点がvに移動したので、stackに追加
+			}
+		} else { // 隣接頂点が見つからなかったら
+			stack.pop()
+			timeCounter++
+			nodes[u].color = black // 隣接頂点が見つからなかったということは、今回の頂点の探索は完了したということ
+			nodes[v].completedTime++
+		}
+	}
+
+}
+
+// getNextNode は隣接する頂点を番号順に取得する。
+func getNextNode(u int) int {
+	for v := nodes[u].value; v < n; v++ {
+		nodes[u].value = v + 1
+		if adjacentMatrix[u][v] { // uが対象の値で、vがuの今回の隣接頂点
+			return v
+		}
+	}
+	return -1
 }
 
 func main() {
