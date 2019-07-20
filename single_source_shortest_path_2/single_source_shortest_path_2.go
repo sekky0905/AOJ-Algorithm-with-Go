51b08052a3d1f38f0624b7f399953d300776b0b7
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-07-21 01:13:31 +0900

    add function of dijkstra

--- single_source_shortest_path_2/single_source_shortest_path_2.go
+++ single_source_shortest_path_2/single_source_shortest_path_2.go
@@ -10,11 +10,16 @@ const (
 )
 
 var (
-	// adjacentMatrix は、隣接行列を表す。
-	adjacentMatrix [][]int
-	nodes          []node
+	// adjacentMatrix は、隣接リストを表す。
+	adjacentMatrix [][]node
 	n              int
 )
+
+// node は、頂点を表す。
+type node struct {
+	key    int
+	weight int
+}
 
 // element は、priority queueの要素を表す。
 type element struct {
@@ -45,17 +50,61 @@ func newPriorityQueue() priorityQueue {
 	return make(priorityQueue, n)
 }
 
-// node は、頂点を表す。
-type node struct {
+// edge は、頂点を表す。
+type edge struct {
 	distance int // 最短コストを表す。
 	color        // 訪問状態を表す。
 }
 
-// initNodes は、nodesを初期化する。
-func initNodes() {
-	nodes = make([]node, n, n)
+// initEdges は、edgesを初期化する。
+func initEdges() []edge {
+	nodes := make([]edge, n, n)
 	for i := range nodes {
 		nodes[i].color = white
 		nodes[i].distance = infinity
+	}
+	return nodes
+}
+
+// dijkstra は、ダイクストラのアルゴリズムを表す。
+func dijkstra() {
+	pq := newPriorityQueue()
+	edges := initEdges()
+
+	// 0(始点)に訪問する。
+	edges[0].distance = 0
+	elm := &element{
+		key:      0,
+		priority: 0,
+	}
+	pq.push(elm)
+	edges[0].color = gray
+
+	for len(pq) != 0 {
+		f := pq.pop()
+		u := f.key
+		edges[u].color = black // 訪問したことにする
+
+		// 現在の最小値を取り出して、それが最短でなければ今回はスキップする
+		if edges[u].distance < f.priority*-1 {
+			continue
+		}
+
+		for j := 0; j < len(adjacentMatrix[u]); j++ { // リスト[index]を回る
+			v := adjacentMatrix[u][j].key
+			if edges[v].color == black {
+				continue
+			}
+
+			if edges[v].distance > edges[u].distance+adjacentMatrix[u][j].weight {
+				edges[v].distance = edges[u].distance + adjacentMatrix[u][j].weight
+			}
+
+			pq.push(&element{
+				key:      v,
+				priority: edges[v].distance * -1,
+			})
+			edges[v].color = gray
+		}
 	}
 }
