e6b244f622f9d2ef4f0b732ef6474ee125ca9877
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-07-25 00:36:26 +0900

    add function to calc matrix chain multiplication

---
+++ matrix_chain_multiplication/matrix_chain_multiplication.go
@@ -0,0 +1,29 @@
+package main
+
+func calcMatrixChainMultiplication(n int, p []int, m [][]int) [][]int {
+	for i := 1; i <= n; i++ {
+		m[i][i] = 0
+	}
+
+	for l := 2; l <= n; l++ {
+		for i := 1; i <= n-1; i++ {
+			j := i + l - 1
+			m[i][j] = 1 << 21
+			for k := i; k <= j-1; k++ {
+				m[i][j] = min(m[i][j], m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j])
+			}
+		}
+	}
+	return m
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+
+}
